dd821d3f653fdf21d5862780d014f57100ee2260
xxeniaev <xenia-ev2502@yandex.ru>
2021-05-17 23:31:23 +0500

    refactor

--- alg.py
+++ alg.py
@@ -6,21 +6,16 @@ class DijkstraSearch:
         self.graph = graph
         self.results = tuple()
 
-    def shortest_path(self, source, sink):
-        # create a priority queue and hash set to store visited nodes
+    def shortest_path(self, source, dest):
         queue, visited = [(0, source, [])], set()
         heapq.heapify(queue)
-        # traverse graph with BFS
         while queue:
             (cost, node, path) = heapq.heappop(queue)
-            # visit the node if it was not visited before
             if node not in visited:
                 visited.add(node)
                 path = path + [node]
-                # hit the sink
-                if node == sink:
+                if node == dest:
                     self.results = (cost, path)
-                # visit neighbours
                 for c, neighbour in self.graph.points[node]:
                     if neighbour not in visited:
                         heapq.heappush(queue, (cost + c, neighbour, path))
@@ -33,6 +28,5 @@ class DijkstraSearch:
         else:
             f = open("out.txt", "a")
             f.write("Y\n" + " ".join(map(str, [x+1 for x in self.results[1]])) + "\n" + str(self.results[0]) + "\n")
-            print("Y\n" + " ".join(map(str, [x+1 for x in self.results[1]])) + "\n" + str(self.results[0]) + "\n")
             f.close()
 
